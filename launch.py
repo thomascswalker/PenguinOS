3d5b4cf727edea2576b983964aff89d7c604b055
thomascswalker <thomascswalker@gmail.com>
2025-04-13 16:55:45 -0400

    Update launch.py

--- launch.py
+++ launch.py
@@ -8,7 +8,7 @@ import sys
 
 OS_NAME = "PengOS"
 
-PATH = os.path.expanduser("~/opt/cc_i386/bin")
+PATH = "~/opt/cc_i386/bin"  # os.path.expanduser("~/opt/cc_i386/bin")
 ARCH = "i386"
 HOST = "i686-elf"
 GCC = os.path.join(PATH, f"{HOST}-g++")
@@ -25,8 +25,12 @@ ISO_FILE = f"{BUILD_DIR}/boot/{OS_NAME}.iso"
 """The ISO image of the OS."""
 DISK_FILE = f"{BUILD_DIR}/disk.img"
 
-GCC_FLAGS = "-O0 -m32 -g -ffreestanding -Wall -Wextra -fno-omit-frame-pointer"
-NASM_FLAGS = "-felf32 -g"
+OPTIMIZE = "-O0"
+DEBUG = "-g"
+STD_VERSION = "-std=c++23"
+GCC_FLAGS = f"-m32 {DEBUG} {OPTIMIZE} -ffreestanding -Wall -fno-omit-frame-pointer -Wno-unused-function -Wno-unused-parameter -Wno-unused-variable -Wno-parentheses -fno-exceptions"
+NASM_FLAGS = f"-felf32 {DEBUG}"
+LD_FLAGS = "-m elf_i386"
 
 HDD_SIZE = 100
 MEM_SIZE = 32
@@ -60,10 +64,10 @@ def verify_file(file: str) -> None:
         sys.exit(1)
 
 
-def get_files_of_type(file_type: str) -> list:
+def get_files_of_type(root: str, file_type: str) -> list:
     files = (
         subprocess.check_output(
-            f"find {SOURCE_DIR} -type f -name '*.{file_type}'", shell=True
+            f"find {root} -type f -name '*.{file_type}'", shell=True
         )
         .decode()
         .splitlines()
@@ -71,8 +75,14 @@ def get_files_of_type(file_type: str) -> list:
     return files
 
 
+def get_programs() -> list:
+    programs = os.listdir("./programs/")
+    root = os.path.dirname(__file__)
+    return [p for p in programs if os.path.isdir(os.path.join(root, "programs", p))]
+
+
 def run_cmd(command: str) -> str:
-    # logger.debug(f"Running command: {command}")
+    logger.debug(f"Running command: {command}")
     result = subprocess.run(command, shell=True, text=True)
     if result.returncode != 0:
         logger.error(f"Command failed: {command}")
@@ -106,7 +116,7 @@ class CLI:
 
         if args.rebuild:
             logger.info("Building kernel...")
-            self.build()
+            self.all()
             logger.info("Kernel build complete.")
 
         if self._clean:
@@ -152,14 +162,13 @@ class CLI:
         verify_file(output_file)
         return output_file
 
-    def compile(self, file: str) -> str:
+    def compile(self, file: str, include: bool = True) -> str:
         basename = os.path.splitext(os.path.basename(file))[0]
         output_file = os.path.join(BUILD_DIR, basename + "_cpp.o")
         include_arg = self.get_include_arg()
         cmd = (
-            f"{GCC} -std=c++23 {GCC_FLAGS} -nostdlib -fno-builtin "
-            f"-Wno-unused-function -Wno-unused-parameter -Wno-unused-variable "
-            f"-Wno-parentheses -fno-exceptions  {include_arg} -c {file} -o {output_file}"
+            f"{GCC} {STD_VERSION} {GCC_FLAGS} -nostdlib -fno-builtin "
+            f"{include_arg if include else ''} -c {file} -o {output_file}"
         )
         run_cmd(cmd)
         verify_file(output_file)
@@ -169,8 +178,7 @@ class CLI:
         logger.debug("Linking object files...")
         kernel_file = os.path.join(BUILD_DIR, f"boot/{name}.bin")
         cmd = (
-            f"{LD} -T {SOURCE_DIR}/arch/{ARCH}/linker.ld "
-            f"-g -nostdlib "
+            f"{LD} {LD_FLAGS} -T {SOURCE_DIR}/arch/{ARCH}/linker.ld "
             f"{' '.join(object_files)} -o {kernel_file} "
         )
         run_cmd(cmd)
@@ -181,11 +189,42 @@ class CLI:
     def build_lib(self, name: str, object_files: list[str]) -> str:
         logger.debug(f"Creating library {name}...")
         lib_file = os.path.join(BUILD_DIR, f"{name}.a")
-        cmd = f"{AR} rcs {lib_file} {' '.join(object_files)}"
+        cmd = f"{AR} r {lib_file} {' '.join(object_files)}"
         run_cmd(cmd)
         verify_file(lib_file)
+        run_cmd(f"ranlib {lib_file}")
         logger.debug(f"Library {name} created.")
         return lib_file
+
+    def build_program(self, directory: str):
+        program_name = os.path.basename(directory)
+        logger.debug(f"Building program {program_name}...")
+
+        obj_files = []
+        for f in get_files_of_type(directory, "cpp"):
+            basename = os.path.splitext(os.path.basename(f))[0]
+            obj_file = os.path.join(BUILD_DIR, basename + "_cpp.o")
+            include_arg = "-I./source/libc -I./source/libcpp"
+            cmd = (
+                f"{GCC} {STD_VERSION} {GCC_FLAGS} -nostdlib -fno-builtin "
+                f"{include_arg} -L./build -c {f} -o {obj_file} -l:libc.a"
+            )
+            run_cmd(cmd)
+            verify_file(obj_file)
+
+            obj_files.append(obj_file)
+
+        logger.info(obj_files)
+        bin_file = os.path.join(BUILD_DIR, f"{program_name}.bin")
+        cmd = (
+            f"{LD} {LD_FLAGS} -T ./programs/linker.ld "
+            f"-L./build "
+            f"{' '.join(obj_files)} -o {bin_file} -l:libc.a "
+        )
+        result = run_cmd(cmd)
+        logger.info(f"Result: {result}")
+
+        run_cmd(f"cp {bin_file} ./base/")
 
     def build_cfg(self):
         """Creates the GRUB configuration file."""
@@ -225,26 +264,58 @@ class CLI:
         for file in self.object_files:
             run_cmd(f"rm -f {file}")
 
-    def build(self) -> None:
-        self.construct_build_dirs()
-        include_arg = self.get_include_arg()
-        logger.debug(f"Include argument: {include_arg}")
+    def build_std_lib(self):
+        dirs = ["./source/libc", "./source/libcpp"]
+        self.libraries = []
+        obj_files = []
+        for d in dirs:
+            files = []
+            files += get_files_of_type(d, "cpp")
+            for f in files:
+                out_file = self.compile(f)
+                obj_files.append(out_file)
 
+        lib_name = "libc"
+        logger.debug(f"Building {lib_name} with files: {obj_files}")
+        lib_file = self.build_lib(lib_name, obj_files)
+        logger.debug(f"Built library file: {lib_file}")
+        verify_file(lib_file)
+        self.libraries.append(lib_file)
+
+        if self._clean:
+            for f in obj_files:
+                run_cmd(f"rm -f {f}")
+
+    def build_kernel(self):
         # Assmemble .s files
-        self.object_files = []
-        for asm in get_files_of_type("s"):
+        obj_files = []
+        for asm in get_files_of_type("./source", "s"):
             logger.debug(f"Processing assembly file: {asm}")
-            self.object_files.append(self.assemble(asm))
+            obj_files.append(self.assemble(asm))
 
         # Compile .cpp files
-        for cpp in get_files_of_type("cpp"):
+        for cpp in get_files_of_type("./source", "cpp"):
             logger.debug(f"Processing C++ file: {cpp}")
-            self.object_files.append(self.compile(cpp))
-        logger.info(f"Object files: {self.object_files}")
+            obj_files.append(self.compile(cpp))
 
-        kernel = self.build_bin(OS_NAME, self.object_files)
+        logger.info(f"Object files: {obj_files}")
+
+        kernel = self.build_bin(OS_NAME, obj_files)
         assert kernel == KERNEL_FILE, "Kernel file mismatch."
         logger.info(f"Kernel linked to: {KERNEL_FILE}")
+
+        if self._clean:
+            for f in obj_files:
+                run_cmd(f"rm -f {f}")
+
+    def all(self) -> None:
+        self.construct_build_dirs()
+        self.build_std_lib()
+        self.build_kernel()
+
+        logger.info("Building programs...")
+        for d in get_programs():
+            self.build_program(f"./programs/{d}")
 
         self.build_cfg()
         self.build_iso()
